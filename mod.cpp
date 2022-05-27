a14cc80cd5f72225595781705e3654c774893640
Siege <81945479+SiegeToaster@users.noreply.github.com>
2022-05-26 18:25:57 -0700

    update mod path

--- mod.cpp
+++ mod.cpp
@@ -1,5 +1,5 @@
 name = "SIA 3DEN Mission Framework";
 author = "Siege";
 actionName = "GitHub";
-action = "https://github.com/SiegeToaster/SIA_3DEN_FRAMEWORK";
+action = "https://github.com/Soliders-in-Arms-Arma-3-Group/SIA_3DEN_framework";
 overview = "Adds actions to the Eden Editor that allow configuration of the SIA Mission Framework.";
