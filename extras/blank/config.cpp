b5d7703f0c0f2439185340ef53180a940f718b41
Siege <81945479+SiegeToaster@users.noreply.github.com>
2022-06-20 22:46:31 -0700

    Add blank module folder customized the ACE one to match our stuff

---
+++ extras/blank/config.cpp
@@ -0,0 +1,15 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = { "sia3f_main" };
+		author = "Soldiers in Arms";
+        authors[] = { "" };
+		url = "https://github.com/Soliders-in-Arms-Arma-3-Group";
+        VERSION_CONFIG;
+    };
+};
