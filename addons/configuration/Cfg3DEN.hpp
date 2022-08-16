beb2f4ae76e2b061e27b2a55b7889e5b7c0b0786
Liam McKendrick <liammckendirckgaming@gmail.com>
2022-08-16 16:29:36 -0700

    Move radio\Cfg3DEN to configuration\Cfg3DEN #37

--- addons/configuration/Cfg3DEN.hpp
+++ addons/configuration/Cfg3DEN.hpp
@@ -61,4 +61,31 @@ class Cfg3DEN {
 			};
 		};
 	};
+
+	class Group {
+		class AttributeCategories {
+			class GVAR(groupSettings) {
+				displayName = "SIA Framework";
+				collapsed = 0;
+
+				class Attributes {
+					class GVAR(radioChannel) {
+						displayName = "Radio Channel";
+						tooltip = "The channel number this group's personal radio will automatically be set to.";
+						property = QGVAR(radioChannel);
+						control = "Edit";
+						expression = "_this setVariable ['%s',_value,true];";
+
+						defaultValue = "1";
+						
+						unique = 0;
+						validate = "number";
+						typeName = "NUMBER";
+						// only units should have this attribute
+						//condition = "objectControllable";
+					};
+				};
+			};
+		};
+	};
 };
