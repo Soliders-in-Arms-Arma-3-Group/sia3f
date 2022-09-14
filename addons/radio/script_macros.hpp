966743839cf39a7ba426e92fef142292f546f526
Siege <81945479+SiegeToaster@users.noreply.github.com>
2022-09-14 08:34:14 -0700

    update settings macro

--- addons/radio/script_macros.hpp
+++ addons/radio/script_macros.hpp
@@ -2,9 +2,15 @@
 
 #define SPATIALIZATION_SETTINGS_VALUE_INFO [[-1, 0, 1], ["Left", "Both", "Right"], 1]
 #define SPATIALIZATION_SETTINGS(RADIO,displayName) [ \
-QGVAR(CONCAT_2(spatializationZone,RADIO)), \
-"LIST", \
-displayName, \
-["SIA Mission Framework", "Spatialization Settings"], \
-SPATIALIZATION_SETTINGS_VALUE_INFO, \
-false] call CBA_fnc_addSetting
+	QGVAR(CONCAT_2(spatializationZone,RADIO)), \
+	"LIST", \
+	displayName, \
+	["SIA Mission Framework", "Spatialization Settings"], \
+	SPATIALIZATION_SETTINGS_VALUE_INFO, \
+	false, \
+	{ \
+		TRACE_2("settings expression",QGVAR(CONCAT_2(spatializationZone,RADIO),_this); \
+	}, \
+	false \
+] call CBA_fnc_addSetting
+// ["sia3f_settingChanged", [QGVAR(CONCAT_2(spatializationZone,RADIO), _this]] call CBA_fnc_localEvent;
