784939c2b4febdce43f8d16a261f20a67d110617
SiegeToaster <81945479+SiegeToaster@users.noreply.github.com>
2022-09-19 11:42:28 -0700

    update radio macros

--- addons/radio/script_macros.hpp
+++ addons/radio/script_macros.hpp
@@ -1,15 +1,16 @@
-#define CONCAT_2(var1,var2) var1####var2
+#define RADIO_VAR(radio) ACRE_radio
 
-#define SPATIALIZATION_SETTINGS_VALUE_INFO [[-1, 0, 1], ["Left", "Both", "Right"], 1]
+#define SPATIALIZATION_SETTINGS_VALUE_INFO [[0, 1, 2], ["Left", "Both", "Right"], 1]
 #define SPATIALIZATION_SETTINGS(RADIO,displayName) [ \
-	QGVAR(CONCAT_2(spatializationZone,RADIO)), \
+	QGVAR(RADIO_VAR(RADIO)), \
 	"LIST", \
 	displayName, \
 	["SIA Mission Framework", "Spatialization Settings"], \
 	SPATIALIZATION_SETTINGS_VALUE_INFO, \
 	false, \
 	{ \
-		TRACE_2("settings expression",QGVAR(CONCAT_2(spatializationZone,RADIO),_this); \
+		TRACE_2(QUOTE(ACRE RADIO (displayName) settings updated),_this); \
+		["setRadioDefaultSpatial", [QUOTE(RADIO_VAR(radio)), ["Left", "Both", "Right"] select _this]] call FUNC(acreRadioSetup); \
 	}, \
 	false \
 ] call CBA_fnc_addSetting
